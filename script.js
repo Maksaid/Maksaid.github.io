4d88b1090400cfd6c4341ef19c6367088bc00c30
Maksaid <mapinchuk12@gmail.com>
2023-10-20 13:10:44 +0300

    table

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-const prev = document.querySelector(".prev");
-const next = document.querySelector(".next");
-const carousel = document.querySelector(".carousel-container");
-const track = document.querySelector(".track");
-let width = carousel.offsetWidth;
-let cardWidth = 20;
-let cardPadding = 6;
-let vwPixels = window.innerWidth / 100;
-const remPixels = parseFloat(getComputedStyle(document.documentElement).fontSize);
-let index = 0;
-let cardAmount = document.querySelectorAll(' .card-container').length;
-let initialCardsAmount = window.innerWidth / (20 * remPixels + 6 * vwPixels);
 
 window.addEventListener('scroll', e => {
     document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
