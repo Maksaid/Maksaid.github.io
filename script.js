509e27280df7d75cfa31f41af830df4bd406167f
Maksaid <mapinchuk12@gmail.com>
2023-11-17 13:22:30 +0300

    lab 6 add

--- script.js
+++ script.js
@@ -17,8 +17,45 @@ window.addEventListener('load', function() {
 });
 
 const logInButton = document.getElementById("log-in");
+
 logInButton.addEventListener('click', function (){
-    fetch('https://jsonplaceholder.typicode.com/users/1')
-        .then(response => response.json())
-        .then(json => console.log(json))
+    let random_id = Math.floor(Math.random() * 10 + 1)
+    document.getElementById('data-container').style.display = 'block';
+    document.getElementById('preloader').style.display = 'block';
+    setTimeout(function (){
+        fetch('https://jsonplaceholder.typicode.com/users/' + random_id)
+            .then(response => response.json())
+            .then(data => {
+                updateContainer(data);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+                document.getElementById('preloader').style.display = 'none';
+            });
+    },2000)
+
 })
+
+
+function updateContainer(data) {
+    const container = document.getElementById('data-container');
+
+    // Add content to the container
+    container.innerHTML = `
+    <button id="close-button" style="width: fit-content;align-self: end; color: red; z-index: 5002; display: block" onclick="closeContainer()">X</button>
+    <h2>${data.name}</h2>
+    <p>Email: ${data.email}</p>
+    <p>Id: ${data.id}</p>
+    <p>Username: ${data.username}</p>
+    <p>Phone: ${data.phone}</p>
+  `;
+
+}
+
+function closeContainer() {
+    // Hide the container when the close button is clicked
+    document.getElementById('data-container').style.display = 'none';
+    closeButton.style.display = 'none';
+    const container = document.getElementById('data-container');
+    container.innerHTML = ''
+}
